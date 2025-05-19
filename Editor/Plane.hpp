058400ef1c8765c48dfded34a554266aa93353db
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-19 09:10:14 -0500

    Create Plane.hpp

---
+++ Editor/Plane.hpp
@@ -0,0 +1,33 @@
+//Plane.hpp
+
+#ifndef PLANE_HPP
+#define PLANE_HPP
+
+#include <string>
+#include "MathSdk.h"
+
+using PlaneVector = vec3_t;
+
+namespace PlaneSDKModule
+{
+
+class Plane{
+public:
+   Plane();
+   virtual ~Plane();
+
+   PlaneVector Dist;
+   PlaneVector Normal;
+
+   int Point( int * i );
+
+   virtual void PlaneSetNormalZero(Plane& plane, PlaneVector NZ = 0.0f){
+         for( auto& i, i >= 0; i++ )
+   } = 0;
+   virtual void PlaneShader(Plane& plane, const std::string& ShaderName, bool bDetail) = 0;
+   virtual void PlaneConsturct(Plane& plane, PlaneVector x, y, z, PlaneVector d) = 0;
+}
+
+}
+
+#endif
