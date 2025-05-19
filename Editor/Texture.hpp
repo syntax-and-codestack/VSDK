1f9d670a7584b3c39478dc850d5cb51a817440f9
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-19 09:38:41 -0500

    Create Texture.hpp

---
+++ Editor/Texture.hpp
@@ -0,0 +1,34 @@
+//Texture.hpp
+
+#ifndef TEXTURE_HPP
+#define TEXTURE_HPP
+
+#include <string>
+#include "MathSdk.h"
+
+#define TEXTURE_X_SCALE 15.0f
+#define TEXTURE_Y_SCALE 15.0f
+
+class CEditorTexture{
+public:
+      CEditorTexture();
+      virtual ~CEditorTexture();
+
+      float CTexureX[15];
+      float CTextureY[15];
+
+      const std::string CTextureName();
+
+      virtual bool CTextureResetScale() = 0;
+      virtual void CTextureFitScale() = 0;
+      virtual void CTextureLMapScale() = 0;
+      virtual float CTextureAlphaTransparency() = 0;
+
+      virtual vec3_t CTextureMatrix(vec3_t CMat4x4[16]){
+             return CMat4x4;
+      } = 0;
+
+
+};
+
+#endif
