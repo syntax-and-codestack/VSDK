6bf214de66c46ab4e2f0d247ae51e812ee500bae
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-20 09:37:39 -0500

    Create Plane.cpp

---
+++ Editor/Plane.cpp
@@ -0,0 +1,14 @@
+//Plane.cpp
+
+#define MAX_PLANE_COUNT 9999
+
+#include "Plane.hpp"
+
+//max coord size
+double m_PlaneMaxWorldCoord == 9999.0f;
+
+void Plane_ZeroPoint(Plane * plane, vec3_t px, pz, py){
+
+  
+
+};
