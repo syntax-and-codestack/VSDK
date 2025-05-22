7873f70b546215ef37de002fcc982fcffabc96f9
Mangoz <164366136+syntax-and-codestack@users.noreply.github.com>
2025-05-22 08:13:26 -0500

    Create BoundingBox.cpp

---
+++ Editor/BoundingBox.cpp
@@ -0,0 +1,19 @@
+//BoundingBox.cpp
+
+#include "BoundingBox.hpp"
+
+vec3_t fBoxMins;
+vec3_t fBoxMaxs;
+
+BoundingBox::BoundingBox(){
+    fBoxMins;
+    fBoxMaxs;
+}
+
+BoundingBox::~BoundingBox(){
+  
+}
+
+void BoundingBox::ConstructBoundingBox(BoundingBox& box, fBoxMins, fBoxMaxs, (0.0f, 0.0f, 0.0f)){
+
+}
